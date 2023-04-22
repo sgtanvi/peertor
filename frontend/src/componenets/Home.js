2240054b5f95eeab3d26db07842985cb53d75816
Het Tikawala <het@Hets-MacBook-Air.local>
2023-04-22 14:23:46 -0700

    Home MVP

--- frontend/src/componenets/Home.js
+++ frontend/src/componenets/Home.js
@@ -4,13 +4,18 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="container">
-
-      <Paper elevation={3} />
-      <div style={{alignItems:'center'}} className="color-panel"></div>
-      <Avatar  alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-      <div className="content">
-        {/* add main content here */}
+    <div className="app">
+      <div className="big-block">
+      </div>
+      <Avatar className='round-icon' alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <div className="main">
+        <div className="box"></div>
+        <div className="content">
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </div>
       </div>
     </div>
   );
