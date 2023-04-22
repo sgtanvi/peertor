4e762681a08bb8c2952116399e3aa48eeed120d6
Het Tikawala <het@Hets-MacBook-Air.local>
2023-04-22 13:54:54 -0700

    changing home.js

--- frontend/src/componenets/Home.js
+++ frontend/src/componenets/Home.js
@@ -1,10 +1,17 @@
+import { Avatar, Paper } from '@mui/material';
 import React from 'react';
+
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-      <p>Welcome to my website!</p>
+    <div className="container">
+
+      <Paper elevation={3} />
+      <div style={{alignItems:'center'}} className="color-panel"></div>
+      <Avatar  alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <div className="content">
+        {/* add main content here */}
+      </div>
     </div>
   );
 }
