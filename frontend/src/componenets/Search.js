7d56fa3b1862c753d93d3d245705bd5659271350
Het Tikawala <het@Hets-MacBook-Air.local>
2023-04-22 16:12:32 -0700

    Search MVP

--- frontend/src/componenets/Search.js
+++ frontend/src/componenets/Search.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Autocomplete, TextField } from '@mui/material';
 
 function Search() {
+
+  const classOptions = ['MATH 39', 'CS146', 'LING21']
   return (
-    <div>
-      <h2>About</h2>
-      <p>This is the About page.</p>
+    <div style={{position:"flex", margin:"20px"}}>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={classOptions}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Class" />}
+      />
     </div>
   );
 }
